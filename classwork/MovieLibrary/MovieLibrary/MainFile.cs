4da217245c22bd3088d484994b34d9686194f008
brandon <brandon.houston896@NEITD-111502>
2020-02-03 21:42:56 -0600

    mon 2/3

---
+++ classwork/MovieLibrary/MovieLibrary/MainFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MovieLibrary.Business;
+
+namespace MovieLibrary
+{
+    public partial class MainFile : Form
+    {
+        public MainFile ()
+        {
+            InitializeComponent();
+
+            Movie movie;
+        }
+
+        private void FileToolStripMenuItem_Click ( object sender, EventArgs e )
+        {
+
+        }
+    }
+}
